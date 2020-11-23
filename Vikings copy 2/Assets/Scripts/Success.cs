5194c0728bd69bbdf1e51e963605f8e76437fa7b
Oliver Qin <oliver.qin1@gmail.com>
2020-11-23 05:09:46 -0500

    Updated Vikings Game
    Full gameplay available

---
+++ Vikings copy 2/Assets/Scripts/Success.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Success : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.tag == "Player")
+        {
+            SoundManager.S.MakeGameWonSound();
+            GameManager.S.PlayerWon();
+        }
+    }
+}
